6af1442bad60b96c7bdbdaf7c1c9e894864dd840
m-u-r-r-a-y <5gwwrm5@gmail.com>
2024-07-10 12:29:57 +0100

    again added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-b^8qsg0xyga%(p7^rj&4$0uqh6*4vgn*akui51eda=!9*sv(5('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
